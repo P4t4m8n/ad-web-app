92b40266dfb4936f3c9ad6c411cdaaf7e66aea85
Eran <michaelieran@gmail.com>
2026-08-15 15:19:48 +0300

    Remove dynamic rendering

--- app/(root)/portraits/page.tsx
+++ app/(root)/portraits/page.tsx
@@ -1,9 +1,6 @@
 import Gird from "@/components/Gird";
 import { getItemsByCategory } from "@/utils/items.server";
 
-// avoid hitting the DB during the build's static generation step
-export const dynamic = "force-dynamic";
-
 export default async function PortraitsPage() {
   const items = await getItemsByCategory("portraits");
 
